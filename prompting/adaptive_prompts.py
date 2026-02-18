d1e455571d8a83c0284fa717a77f0a9e8b929af2
davisrbr <davisbrownr@gmail.com>
2026-02-17 19:00:16 -0500

    Prune non-paper pipelines and residual utility code

--- prompting/adaptive_prompts.py
+++ prompting/adaptive_prompts.py
@@ -266,7 +266,7 @@ class AdaptiveJailbreakRetrieval:
 
 
 class AdaptiveTruthfulQARetrieval:
-    '''This assumes that the dataset already has embeddings. See data/dataset_embedder.py for an example of how to add embeddings to the dataset.'''
+    '''This assumes the dataset already contains embedding vectors in sample metadata.'''
     def __init__(self, incorrect_samples: List[Sample], correct_samples: List[Sample], embeddings_model_name: str = None):
         self.incorrect_samples = incorrect_samples
         self.correct_samples = correct_samples
